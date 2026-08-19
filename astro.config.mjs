fbf422be46dbc39faf7888512f79cd325aa4c0b0
increaseroasir <alex@increaseroas.com>
2026-08-19 01:29:01 -0400

    add: astro.config.mjs

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
+
+export default defineConfig({
+  adapter: node({
+    mode: 'standalone',
+  }),
+  session: false,
+});
